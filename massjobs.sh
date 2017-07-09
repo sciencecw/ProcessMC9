9770d2b06a8e972428e1087281781269a621ecbd
Sarah <eno@umd.edu>
2017-07-09 19:20:20 -0400

    ha

--- massjobs.sh
+++ massjobs.sh
@@ -1,3 +1,1 @@
-condor_submit files_QCD/condor-jobs-0_files-QCD-1.jdl
-condor_submit files_QCD/condor-jobs-0_files-QCD-2.jdl
-condor_submit files_QCD/condor-jobs-0_files-QCD-3.jdl
+condor_submit files_QCD/condor-jobs-1_files-QCD-1.jdl
---
+++ toyMC_bck.C
@@ -0,0 +1,25 @@
+/*
+
+*/
+
+#include "TH1.h"
+#include "TSystem.h"
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+
+
+int idbg=0;
+float eff_b_true = 0.3;
+  float eff_l_true = 0.001;
+
+void toyMC_bck()
+{
+
+
+
+
+}
+
+//------------------------------------------------------------------------------
